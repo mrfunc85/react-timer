4f58195b7517c203598ee3e91de3343993d698b5
MrFunc <mrfunc@mrfunc.com>
2022-12-06 07:28:12 -0800

    In the message box

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Another Remote Update Sourcetree
+          Learn React
         </a>
       </header>
     </div>
